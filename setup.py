84b630826eeeae237c2167a5cb198ed9d38cc6ba
John Wass <wassj@ctc.com>
2020-03-02 23:28:24 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import os
+from setuptools import setup, find_packages
+
+requires = []
+with open(os.path.join(os.path.dirname(__file__), 'requirements.txt')) as f:
+    requires = f.read().splitlines()
+
+setup(name='yolo_aug',
+      version='0.1.0',
+      packages=find_packages(),
+      install_requires=requires,
+      include_package_data=True
+      )
